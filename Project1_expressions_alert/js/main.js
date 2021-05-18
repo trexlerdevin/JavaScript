2da4c94054e1261290bd02dd9a49c479fd746e87
trexlerdevin <trexlerdevin@gmail.com>
2021-05-18 16:44:34 -0700

    committing changes to files in js

---
+++ Project1_expressions_alert/js/main.js
@@ -0,0 +1,27 @@
+var A = " this is a string."; //assigning variable "A" a value of " this is a string"
+var A = A.fontcolor("red"); //assigning variable A font color red
+
+var x = "So You have chosen to join the debate.", z = " or have you have chosen incorrectly"; // assigned variable x a value 
+
+window.alert(x); //added window alert for variable x
+
+var blues = "I have the ", feeling = " blues"; //assigned variable blues and feeling a string value
+var feeling = feeling.fontcolor("blue"); //assigned variable feeling a font color blue
+document.write(blues,feeling); //document write statement for vaiables blues and feeling
+
+
+
+document.write(A); //statement to write var A
+
+document.write(" \"Devin said that he is awesome, and that he\'s the best!\"" // concatenated a string
++ " Ana disagrees and beleives that shes\'s awesome and is the best"
++ " Lucy thinks that both Ana and Devin are both awesome, but that she\'s the best.");
+
+var f = " Devin is right"; //assigned var f a string value
+var f = f.fontcolor("green"); //assigned var f a font color  green
+
+var d = " Devin thinks that," + " Ana, and Lucy are both wrong though. Devin is awesome and is the best!"; // concatenated var d with 2 string values
+var d = d.fontcolor("purple"); // assigned var d a font color of pruple
+document.write(d + f); //concatenated variables d, and f
+
+document.write(3435 + 32445); //statement to perform expression and return sum of two numbers
