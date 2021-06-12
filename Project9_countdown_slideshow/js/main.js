bc6e3a0bf345ae08d97c06c154a3a93a4aecc704
trexlerdevin <trexlerdevin@gmail.com>
2021-06-11 17:18:17 -0700

    committing changes to Project9 folder

---
+++ Project9_countdown_slideshow/js/main.js
@@ -0,0 +1,43 @@
+function countdown() { // this is my countdown function
+    var seconds = document.getElementById("seconds").value;
+        
+    function tick() {
+        seconds = seconds - 1;
+        timer.innerHTML = seconds;
+        setTimeout(tick, 1000);
+    if(seconds == -1) {
+        alert("Times up!");
+    }
+        
+        }
+    tick();
+}
+
+var slideIndex = 1; // this is my slidshow effects function
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n)
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if(n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i <dots.length; i ++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
