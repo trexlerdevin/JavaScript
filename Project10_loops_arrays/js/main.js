f823116e44f05800a88d15c423a3ba54b14445da
trexlerdevin <trexlerdevin@gmail.com>
2021-06-14 15:57:01 -0700

    committing changes to Project10 folder

--- Project10_loops_arrays/js/main.js
+++ Project10_loops_arrays/js/main.js
@@ -1,4 +1,4 @@
-function counting_to_ten() {
+function counting_to_ten() { // This is my whlie loop function
     var digit = "";
     var x = 1;
     while (x <11) {
@@ -8,7 +8,7 @@ function counting_to_ten() {
     document.getElementById("counting to ten").innerHTML = digit
 }
 
-var instruments = ["guitar", "drums", "bass", "piano"];
+var instruments = ["guitar", "drums", "bass", "piano"]; // this is my for loop function
 var X = "";
 var z;
 function for_loop() {
@@ -18,7 +18,7 @@ function for_loop() {
     document.getElementById("list of instruments").innerHTML = X;
 }
 
-function array_function(){
+function array_function(){ // this is my array function
     var action = [];
     action[0] = "running";
     action[1] = "walking";
@@ -27,13 +27,14 @@ function array_function(){
     document.getElementById("actions").innerHTML = "This person is " + action[2] + " very fast!";
 }
 
-function constant_function() {
+function constant_function() { // this is my constant function
     const pet ={type: "cat", breed: "maine coon", color: "black and grey"};
     pet.breed = " tabby";
     pet.color = "grey and tan ";
     document.getElementById("constant").innerHTML = "The pet at the petshop that is avilable for adoption is a " + pet.color + pet.breed + ".";
 }
 
+// this is my let function
 var x = "hello there....";
 document.write(x);
 {
@@ -42,6 +43,7 @@ document.write(x);
 }
 document.write("<br>" + x);
 
+// this is my let object function
 let smartphone = {
     make : "Samsung ",
     model : "Galaxy S21 ",
@@ -52,3 +54,29 @@ let smartphone = {
         }      
 };
 document.getElementById("object smartphone").innerHTML = smartphone.description();
+
+// this is my break statement function
+function break_statement(){
+    var greeting = "";
+    var response = x;
+    for (response = 0; response < 9; response ++) {
+        if (response === 7){
+        break;
+    }
+    greeting += "hello there " + response + "<br>";
+    }
+    document.getElementById("break").innerHTML = greeting;
+}
+
+// this is my continue statement function
+function continue_statement() {
+    var x = "";
+    var y;
+    for (y = 0; y < 8; y ++) {
+        if (y === 5) {
+            continue
+        }
+        x += "The number is " + y + "<br>";
+    }
+    document.getElementById("continue").innerHTML = x
+}
